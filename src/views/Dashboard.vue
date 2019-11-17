9243817069c780fc984ac6b8aebb2d0999b3ef20
Joe Becher <jwbecher@drazisil.com>
2019-11-17 10:34:43 -0500

    start of dashboard

---
+++ src/views/Dashboard.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="dashboardt">
+    <h1>This is the dashboard page</h1>
+    <h2>{{ message }}</h2>
+  </div>
+</template>
+
+<script>
+const axios = require("axios").default;
+
+export default {
+  name: "dashboard",
+  data() {
+    return {
+      message: ""
+    };
+  },
+  async mounted() {
+    const response = await axios.get("http://localhost:4000");
+    this.message = response.data.message;
+  }
+};
+</script>
